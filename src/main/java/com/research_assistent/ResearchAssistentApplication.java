5fe4527fd8823734e0bb949c1817b655d688bab5
manish9548 <baghelmanish4947@gmail.com>
2026-05-27 19:30:33 +0530

    Initial commit

---
+++ src/main/java/com/research_assistent/ResearchAssistentApplication.java
@@ -0,0 +1,13 @@
+package com.research_assistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchAssistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchAssistentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/research_assistent/ResearchAssistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research_assistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchAssistentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
