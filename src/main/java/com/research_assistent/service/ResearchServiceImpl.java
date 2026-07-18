e9bf5c8fa1697b57bd28372553b9ec24bae739f2
manish9548 <baghelmanish4947@gmail.com>
2026-07-18 11:25:10 +0530

    refactor: add service implementation, gemini client and prompt builder

--- src/main/java/com/research_assistent/service/ResearchServiceImpl.java
+++ src/main/java/com/research_assistent/service/ResearchServiceImpl.java
@@ -1,100 +1,24 @@
 package com.research_assistent.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.research_assistent.client.GeminiClient;
 import com.research_assistent.dto.GeminiResponse;
 import com.research_assistent.dto.ResearchRequest;
+import com.research_assistent.util.PromptBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.Map;
 
-public class ResearchServiceImpl {
+@Service
+@RequiredArgsConstructor
+public class ResearchServiceImpl implements ResearchService{
 
-    public ResearchService(WebClient.Builder builder, ObjectMapper objectMapper) {
-        this.webClient = builder.build();
-        this.objectMapper = objectMapper;
-    }
+    private final GeminiClient geminiClient;
 
-    public String processContent(ResearchRequest request) {
+    private final PromptBuilder promptBuilder;
 
-        String prompt = buildPrompt(request);
-
-        Map<String, Object> requestBody = Map.of(
-                "contents", List.of(
-                        Map.of(
-                                "parts", List.of(
-                                        Map.of("text", prompt)
-                                )
-                        )
-                )
-        );
-
-        String response = webClient.post()
-                .uri(geminiApiUrl + geminiApiKey)
-                .bodyValue(requestBody)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-
-        System.out.println("========== GEMINI RESPONSE ==========");
-        System.out.println(response);
-        System.out.println("=====================================");
-
-        return extractTextFromResponse(response);
-    }
-
-    private String extractTextFromResponse(String response) {
-
-        try {
-
-            GeminiResponse geminiResponse =
-                    objectMapper.readValue(response, GeminiResponse.class);
-
-            if (geminiResponse.getCandidates() != null &&
-                    !geminiResponse.getCandidates().isEmpty()) {
-
-                GeminiResponse.Candidate candidate =
-                        geminiResponse.getCandidates().get(0);
-
-                if (candidate.getContent() != null &&
-                        candidate.getContent().getParts() != null &&
-                        !candidate.getContent().getParts().isEmpty()) {
-
-                    return candidate.getContent()
-                            .getParts()
-                            .get(0)
-                            .getText();
-                }
-            }
-
-            return "No response generated.";
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error Parsing Response : " + e.getMessage();
-        }
-    }
-
-    private String buildPrompt(ResearchRequest request) {
-
-        StringBuilder prompt = new StringBuilder();
-
-        switch (request.getOperation().toLowerCase()) {
-
-            case "summarize":
-                prompt.append("Summarize the following content:\n\n");
-                break;
-
-            case "suggest":
-                prompt.append("Give suggestions to improve the following content:\n\n");
-                break;
-
-            default:
-                throw new IllegalArgumentException("Invalid Operation");
-        }
-
-        prompt.append(request.getContent());
-
-        return prompt.toString();
-    }
 }
+
