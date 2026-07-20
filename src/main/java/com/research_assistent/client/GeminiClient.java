dd66869324c88b9b57686789fbe57b978d078324
manish9548 <baghelmanish4947@gmail.com>
2026-07-20 08:04:25 +0530

    refactor: separate prompt builder and gemini client responsibilities

--- src/main/java/com/research_assistent/client/GeminiClient.java
+++ src/main/java/com/research_assistent/client/GeminiClient.java
@@ -1,13 +1,43 @@
 package com.research_assistent.client;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class GeminiClient {
 
     private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public String generateContent(String prompt) {
+
+        Map<String, Object> requestBody = Map.of(
+                "contents", List.of(
+                        Map.of(
+                                "parts", List.of(
+                                        Map.of("text", prompt)
+                                )
+                        )
+                )
+        );
+
+        return webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 
 }
--- src/main/java/com/research_assistent/util/PromptBuilder.java
+++ src/main/java/com/research_assistent/util/PromptBuilder.java
@@ -1,8 +1,33 @@
 package com.research_assistent.util;
 
+import com.research_assistent.dto.ResearchRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PromptBuilder {
 
+    private final PromptBuilder promptBuilder;
+
+    public String buildPrompt(ResearchRequest request) {
+
+        StringBuilder prompt = new StringBuilder();
+
+        switch (request.getOperation().toLowerCase()) {
+
+            case "summarize":
+                prompt.append("Summarize the following content:\n\n");
+                break;
+
+            case "suggest":
+                prompt.append("Give suggestions to improve the following content:\n\n");
+                break;
+
+            default:
+                throw new IllegalArgumentException("Invalid Operation");
+        }
+
+        prompt.append(request.getContent());
+
+        return prompt.toString();
+    }
 }
