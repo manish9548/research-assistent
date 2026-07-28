9f0767a110ed83731458e330b917d370d8184807
manish9548 <baghelmanish4947@gmail.com>
2026-07-28 22:10:14 +0530

    refactor:  enum class complete

---
+++ src/main/java/com/research_assistent/enums/OperationType.java
@@ -0,0 +1,13 @@
+package com.research_assistent.enums;
+
+public enum OperationType {
+
+    SUMMARIZE,
+    ANALYZE,
+    KEYWORD_EXTRACTION,
+    LITERATURE_REVIEW,
+    ABSTRACT_GENERATION,
+    CITATION_GENERATION,
+    PARAPHRASE,
+    GRAMMAR_CHECK
+}
